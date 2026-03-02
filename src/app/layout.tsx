077197afa86b72e0792134c321c8d0787ec7bf10
Andrés Sánchez Botta <lsb.creativa@gmail.com>
2026-03-02 09:13:34 -0500

    Add Google Analytics (G-C8CC3XNYFV) to all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 import SearchDialog from "@/components/layout/SearchDialog";
@@ -110,6 +111,16 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
       </head>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-C8CC3XNYFV"
+        strategy="afterInteractive"
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-C8CC3XNYFV');`}
+      </Script>
       <body className="flex min-h-screen flex-col antialiased">
         <ToastProvider>
           <Navbar />
